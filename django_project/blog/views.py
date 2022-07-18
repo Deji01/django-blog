9ef175ea0f26d7b52614b2b65186fcc0af8f28e2
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-18 04:11:36 +0000

    Database & Migratioons

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-# Create your views here.
-
-posts = [
-    {
-    "author":"Ayodeji",
-    "title":"Blog Post 1",
-    "content":"First post content",
-    "date_posted":"July 17th, 2022",
-    },
-   {
-    "author":"Dejavu",
-    "title":"Blog Post 2",
-    "content":"Second post content",
-    "date_posted":"July 18th, 2022",
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-    "posts":posts
+    "posts": Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
