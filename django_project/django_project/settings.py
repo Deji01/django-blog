0411fda5b369b1308607c627e41c27b1f8530f81
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-19 21:04:46 +0000

    User Registration

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -35,6 +35,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
